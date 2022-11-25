28fcab4765634ee549b9cf059b43812cca672137
DanielAbraha <dannyabraha27@gmail.com>
2022-11-25 17:12:52 -0600

    final project

---
+++ waa_final_project/src/main/java/edu/miu/waa_final_project/service/impl/MyUserDetailService.java
@@ -0,0 +1,25 @@
+package edu.miu.waa_final_project.service.impl;
+
+
+import edu.miu.waa_final_project.domain.User;
+import edu.miu.waa_final_project.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class MyUserDetailService implements UserDetailsService {
+    @Autowired
+    private UserRepo userRepo ;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepo.findByEmail(username);
+        var userDetails = new MyUserDetails(user);
+        return userDetails;
+    }
+}
